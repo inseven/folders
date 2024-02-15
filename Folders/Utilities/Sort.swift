84b87dd3f5b2c87e41d24bc43656db28a9c0ba7a
Jason Morley <hello@jbmorley.co.uk>
2024-02-15 11:36:45 -1000

    refactor: Move `Sort` into a separate file

---
+++ Folders/Utilities/Sort.swift
@@ -0,0 +1,71 @@
+// MIT License
+//
+// Copyright (c) 2023-2024 Jason Morley
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+
+import SQLite
+
+protocol Sort {
+
+    func compare(_ lhs: Details, _ rhs: Details) -> Bool
+    var order: [Expressible] { get }
+}
+
+struct DisplayNameAscending: Sort {
+
+    func compare(_ lhs: Details, _ rhs: Details) -> Bool {
+        return lhs.url.displayName.localizedStandardCompare(rhs.url.displayName) == .orderedAscending
+    }
+
+    var order: [Expressible] {
+        return [Store.Schema.name.asc]
+    }
+
+}
+
+extension Sort where Self == DisplayNameAscending {
+
+    static var displayNameAscending: DisplayNameAscending {
+        return DisplayNameAscending()
+    }
+
+}
+
+struct DisplayNameDescending: Sort {
+
+    func compare(_ lhs: Details, _ rhs: Details) -> Bool {
+        return lhs.url.displayName.localizedStandardCompare(rhs.url.displayName) == .orderedDescending
+    }
+
+    var order: [Expressible] {
+        return [Store.Schema.name.desc]
+    }
+
+}
+
+extension Sort where Self == DisplayNameDescending {
+
+    static var displayNameDescending: DisplayNameDescending {
+        return DisplayNameDescending()
+    }
+
+}
